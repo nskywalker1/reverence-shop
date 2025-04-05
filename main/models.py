1c3b8b9cd968a89ff114bd549f1412bf703f3665
Nazar Rachkivskiy <rachkovskiy.ua@gmail.com>
2025-04-05 16:53:47 +0300

    add media, home page, catalog

--- main/models.py
+++ main/models.py
@@ -33,13 +33,13 @@ class ClothingItem(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    discount = models.DecimalField(max_digits=5, decimal_places=2)
+    discount = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
 
     def __str__(self):
         return self.name
 
     def get_price_with_discount(self):
-        if self.discount > 0:
+        if self.discount:
             return self.price * (1 - (self.discount / 100))
         return self.price
 

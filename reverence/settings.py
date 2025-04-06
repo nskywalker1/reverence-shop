5b18e69ae6458ffd1afc8915ef3dfd56379e1c47
Nazar Rachkivskiy <rachkovskiy.ua@gmail.com>
2025-04-06 16:04:20 +0300

    add class cart, add styles for detail product html

--- reverence/settings.py
+++ reverence/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'cart'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+CART_SESSION = 'cart'

ee7b61c7d62d9d7f9a3619a1710776b72992868c
Nazar Rachkivskiy <121402812+nskywalker1@users.noreply.github.com>
2025-04-15 16:49:57 +0300

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -20,10 +20,20 @@ class UserRegistrationForm(UserCreationForm):
         return password2
 
 
-class UserLoginForm(AuthenticationForm):
-    class Meta:
-        model = User,
-        fields = ('email', 'password')
+class UserLoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+    def clean(self):
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+
+        if email and password:
+            user = authenticate(email=email, password=password)
+            if user is None:
+                raise forms.ValidationError("Invalid email or password.")
+        return self.cleaned_data   
 
 
 class UserProfileForm(forms.ModelForm):
